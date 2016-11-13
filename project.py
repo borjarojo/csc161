933eae26555630af18f8b114ff48c6a1621348a0
borjarojo <borja.rojo@gmail.com>
2016-11-12 22:13:13 -0500

    Chagnes to project.py

--- project.py
+++ project.py
@@ -62,7 +62,8 @@ def col_convert(col):
     elif col == "adj_close":
         return 6
 
-def alg_moving_average():
+# Arguments are optional. Cleared with professor.
+def alg_moving_average(column="close", margin=20, perc_margin=.2):
     # Start by getting an organized copy of the data
     data = read_AAPL_file()
 
@@ -83,10 +84,8 @@ def alg_moving_average():
     Margin: The amount of data being used for the average
     perc_margin: The percentage used to buy and sell stocks
     """
-    column = col_convert("close")
-    margin = 15     # This is the span of the average
+    column = col_convert(column)
     data_margin_total = 0
-    perc_margin = .05
     perc_diff_var = 0
 
     # Get the first span of data totaled
@@ -121,12 +120,64 @@ def alg_moving_average():
 
     return stocks, capital
 
+# Args are optional, cleared with professor.
+def alg_mine(column="close"):
+    """This is a version of the stock trading algorithm that is determined
+    by a custom parameter. The idea is that the current stock price being
+    looked at will be compared to the alphabet. If it is a vowel, sell,
+    otherwise buy.
+
+    This will be done by modding the stock price by 26 and then checking to
+    see if the number returned matches with a vowel.
+
+    A = 0 | E = 4 | I = 8 | O = 14 | U = 21
+
+    This will be evaluated using another function that returns True of False
+    depending on the result: is_vowel
+
+    The data will be run through each day and determine the decision.
+    Specifically, it will buy a stock for each consinent and sell all the
+    stocks for each vowel..
+
+    """
+    
+    data = read_AAPL_file()         # Get organized data
+    column = col_convert(column)    # Set the column
+    capital = account[0]            # Set the capital
+    stocks = account[1]             # Set the stocks
+
+    for i in range(1, len(data)):   # For every peice of data
+        current_stock_price = data[i][column]
+        current_stock_is_vowel = is_vowel(current_stock_price)
+        if current_stock_is_vowel == False and capital >= current_stock_price: 
+            capital -= data[i][column]
+            stocks += 1
+        elif current_stock_is_vowel == True:
+            capital += data[i][column] * stocks
+            stocks = 0
+
+    capital += data[i][column] * stocks
+    stocks = 0
+    
+    return stocks, capital
+    
+def is_vowel(num):
+    modded = (num % 26) // 1 # Mod and floor
+    if modded == 0 or modded == 4 or modded == 8 or modded == 14 or modded == 21:
+        return True
+    return False
+
+
 def perc_diff(num_one, num_two):
     return (num_one / num_two) - 1
 
 def main():
     alg1_stocks, alg1_balance = alg_moving_average()
+    
+    alg2_stocks, alg2_balance = alg_mine()
 
-    print(alg1_stocks, alg1_balance)
+    print("alg_moving_average: Stocks", alg1_stocks," Capital $", alg1_balance)
+    #print("alg_mine: Stocks", alg2_stocks, " Capital $", alg2_balance)
 
-main()
+if __name__ == '__main__':
+    main()
